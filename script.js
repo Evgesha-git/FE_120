6f34bccb7bec1d1ae6d6c21cb9c30275f34ca200
Evgenii <info@myitschool.by>
2022-07-19 20:45:08 +0300

    cw_js_1

---
+++ script.js
@@ -0,0 +1,115 @@
+// let a = 1;
+
+// // let a = 'Error';
+
+// var b = 2;
+
+// {
+//     let a = 3;
+// }
+
+// {
+//     var b = 6;
+// }
+
+// console.log(b);
+
+// const c = 3;
+
+// // c = 5;
+
+// // const arr = [];
+
+// // arr[100] = 34;
+
+// // console.log(arr);
+
+// let str = 'string';
+
+// let num = 2;
+
+// let bool = true || false;
+
+// let obj = {};
+
+// let arr = [];
+
+// let bint = 34;
+
+// let u = undefined;
+
+// let n = null;
+
+// let na = NaN;
+
+// // let s = Symbol;
+
+// // let map = Map;
+
+// // let set = Set();
+
+// console.log(45 - '36427dfs');
+
+
+// console.log(Number('323'));
+
+// console.log(Number('323a'));
+
+// console.log(parseInt('434')); //434
+
+// console.log(parseInt('434a')); //434
+
+// console.log(parseInt('a434')); //NaN
+
+// console.log(parseInt('43s4')); //43
+
+// console.log(parseInt('43.4'));
+
+// console.log(parseFloat('43.4'));
+
+// console.log(+'5');
+
+// let a1 = 5;
+
+// // a1 = a1 + 3;
+
+// a1 += 3;
+
+// a1 *= 6;
+
+// a1 /= 2;
+
+// a1 -= 10;
+
+// a1 %= 3;
+
+// console.log(a1);
+
+
+console.log(0 || 1);
+
+console.log('' || 2);
+
+console.log(null || 3);
+
+console.log(undefined || 4);
+
+console.log('' && 2 && 1);
+
+console.log(null ?? 5);
+
+console.log(undefined ?? 6);
+
+console.log(1 == '1');
+
+console.log(1 === '1');
+
+console.log(1 != '1');
+
+console.log(1 !== '1');
+
+// >=
+
+let vektor = 323;
+
+let firsName
