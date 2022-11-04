802f76e6f500e75d7fb4098f20707d73b31f3399
Evgenii <chico7496@gmail.com>
2022-11-04 20:58:16 +0300

    chat complite

--- test-chat/src/components/Chat/index.jsx
+++ test-chat/src/components/Chat/index.jsx
@@ -1,7 +1,61 @@
+import { useEffect, useState, useContext } from "react";
+import { Context } from "../..";
+import { useCollectionData } from "react-firebase-hooks/firestore";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { collection, addDoc, query, orderBy } from "firebase/firestore";
+import { serverTimestamp } from "firebase/firestore";
+
 
 const Chat = () => {
+    const { auth, firestore } = useContext(Context);
+    const [user] = useAuthState(auth);
+    const [value, setValue] = useState('');
+    let [messages, loading] = useCollectionData(
+        query(collection(firestore, 'messages'), orderBy('createAt'))
+    );
+
+    console.log(messages, loading);
+
+    const sendMessage = async (e) => {
+        e.preventDefault();
+        await addDoc(collection(firestore, 'messages'), {
+            uid: user.uid,
+            displayName: user.displayName,
+            photoUrl: user.photoURL,
+            text: value,
+            createAt: serverTimestamp(),
+        });
+        setValue('');
+    }
+
+    if (loading){
+        return <div>Loading...</div>
+    }
+
     return (
-        <h1>Chat</h1>
+        <div className="container">
+            <div className="chat">
+                {messages.map(message => {
+                    return (
+                        <div key={message.text} style={{
+                            margin: 10,
+                            border: user.uid === message.uid ? '2px solid green' : '2px solid red',
+                            width: 'fit-content',
+                            padding: 5
+                        }}>
+                            <div className="avatar">
+                                <img src={message.photoUrl} alt="" />
+                            </div>
+                            <div className="text">{message.text}</div>
+                        </div>
+                    )
+                })}
+            </div>
+            <form onSubmit={sendMessage}>
+                <textarea onInput={e => setValue(e.target.value)} value={value}></textarea>
+                <button type="submit">Отправить</button>
+            </form>
+        </div>
     )
 }
 
